4b3652175f32c62a152576d176a94239295fe4dd
Azyone <squeld@outlook.fr>
2021-04-26 20:18:13 +0200

    10

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,5 +1,10 @@
-
-let testPostIT = new CPostIT (40,40,"#BA274A",1,1,"test2");
-testPostIT.affPostIT();
-testPostIT.textePostIT("Yo la street");
-testPostIT.affPostIT();
+testPostIT = []
+let compteur = 0;
+function NewPostIT(couleur){
+    compteur += 1;
+    testPostIT.push(new CPostIT (40,40,couleur,1,1,"test2","PostIT"+compteur))
+    let taille = testPostIT.length;
+    for(i = 0; i < taille; i++) {
+        testPostIT[i].affPostIT();
+    }
+}
