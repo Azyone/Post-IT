0cd3ea2fcd67fecc6332b506d78d7d8f707be04d
Azyone <squeld@outlook.fr>
2021-04-26 14:53:11 +0200

    7

--- assets/JS/Post-IT.js
+++ assets/JS/Post-IT.js
@@ -4,23 +4,54 @@ class CPostIT{
     couleur;
     x;
     y;
+    id;
 
-    constructor(largeur, longeur, couleur, x, y){
-        this.largeur = largeur;
-        this.longeur = longeur;
+    constructor(largeur, longeur, couleur, x, y, texte, id){
+        this.largeur = 100;
+        this.longeur = 100;
         this.couleur = couleur;
-        this.x = x;
-        this.y = y;
+        this.x = 500;
+        this.y = 500;
+        this.texte = "";
+        this.id = id;
 
 
     }
     //fonctions
     affPostIT(){
-        let PostIT = document.createElement('div');
+        let PostIT = document.getElementById(this.id);
+        if(PostIT === null){
+            PostIT = document.createElement('div');
+            PostIT.id = this.id;
+            document.body.appendChild(PostIT);
+        }
+        
         PostIT.setAttribute('class','PostIT');
         PostIT.style.width =(this.largeur)+"px";
         PostIT.style.height =(this.longeur)+"px";
+        PostIT.style.left =(this.x)+"px";
+        PostIT.style.top =(this.y)+"px";
+        PostIT.style.position ="fixed";
         PostIT.style.backgroundColor =(this.couleur);
-        conteneur.appendChild(PostIT)
+        PostIT.innerHTML =(this.texte);
+        
+    }
+
+    movePostIT(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    colorPostIT(couleur){
+        this.couleur = couleur;
+    }
+
+    redimPostIT(largeur, longeur){
+        this.largeur = largeur;
+        this.longeur = longeur;
+    }
+
+    textePostIT(texte){
+        this.texte = texte;
     }
 }
--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,5 +1,5 @@
 
-let conteneur = document.getElementById('test')
-
-let testPostIT = new CPostIT (40,40,"red",1,1);
+let testPostIT = new CPostIT (40,40,"#BA274A",1,1,"test2");
+testPostIT.affPostIT();
+testPostIT.textePostIT("Yo la street");
 testPostIT.affPostIT();
