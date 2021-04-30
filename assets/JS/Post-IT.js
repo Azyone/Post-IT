9a8cc77076a7c62c52058504245c970c2d532448
Azyone <squeld@outlook.fr>
2021-04-30 15:01:18 +0200

    v11

--- assets/JS/Post-IT.js
+++ assets/JS/Post-IT.js
@@ -1,3 +1,9 @@
+/**
+*@author Aymeric Fischer
+*@version 1.0
+*@class classe de post-it
+*/
+
 class CPostIT{
     largeur;
     longeur;
@@ -8,6 +14,17 @@ class CPostIT{
     isDrawing;
     monInterval;
 
+    /**
+     * Constructeur de post-it
+     * @param {number} largeur largeur du post-it
+     * @param {number} longeur largeur du post-it
+     * @param {string} couleur couleur primaire du post-it
+     * @param {string} couleur2 couleur secondaire du post-it
+     * @param {number} x coordonnée x du post-it
+     * @param {number} y coordonnée y du post-it
+     * @param {string} texte texte contenu dans le post-it
+     * @param {number} id id du post-it
+     */
     constructor(largeur, longeur, couleur, couleur2, x, y, texte, id){
         this.largeur = 100;
         this.longeur = 100;
@@ -22,17 +39,16 @@ class CPostIT{
 
 
     }
-    //fonctions
+    /**
+     * Foncton permetant d'afficher un post-it sur le navigateur
+     */
     affPostIT(){
         let PostIT = document.getElementById(this.id);
         if(PostIT === null){
             PostIT = document.createElement('div');
             PostIT.id = this.id;
             document.body.appendChild(PostIT);
-            //this.movePostIT();
-            //this.redimPostIT();
         }
-        
         PostIT.setAttribute('class','postIT2');
         PostIT.style.width =(this.largeur)+"px";
         PostIT.style.minWidth= "100px"
@@ -41,8 +57,11 @@ class CPostIT{
         PostIT.style.left =(this.x)+"px";
         PostIT.style.top =(this.y)+"px";
         PostIT.style.position ="fixed";
+        PostIT.style.wordWrap ="break-word";
+        PostIT.style.overflow ="hidden";
         PostIT.style.backgroundColor =(this.couleur);
         PostIT.style.color =(this.couleur2);
+        PostIT.style.fontFamily ="p2";
         PostIT.innerHTML =this.texte;
         let menu = document.createElement('div');
         menu.style.height='20px';
@@ -61,10 +80,8 @@ class CPostIT{
         boutDeplace.style.margin='5px';
         boutDeplace.addEventListener('click', (e) => {
             move = true;
+            id = this.id;
             e.stopPropagation()
-            if(move == true){
-                this.movePostIT();
-            }
         })
         menu.appendChild(boutDeplace);
         let boutRedim = document.createElement('i');
@@ -72,14 +89,12 @@ class CPostIT{
         boutRedim.classList.add('fa-compress');
         boutRedim.addEventListener('click', (e) => {
             resize = true;
+            id = this.id;
             this.initMouseX = mouseX;
             this.initmouseY = mouseY;
             this.initlargeur = this.largeur;
             this.initlongeur = this.longeur;
             e.stopPropagation()
-            if(resize == true){
-                this.redimPostIT();
-            }
         })
         menu.appendChild(boutRedim);
         let boutEdit = document.createElement('i');
@@ -87,66 +102,47 @@ class CPostIT{
         boutEdit.classList.add('fa-edit');
         boutEdit.addEventListener('click', (e) => {
             edit = true;
+            id = this.id;
             e.stopPropagation()
-            if(edit == true){
-                this.textePostIT();
-            }
         })
         menu.appendChild(boutEdit);
     }
 
+    /**
+     * Foncton permetant de déplacer 1 post-it
+    */
     movePostIT(){
-        /*document.getElementById(this.id).addEventListener('mousedown', e => {
-            this.isDrawing = true;
-            this.monInterval = setInterval(()=>{
-                if(this.isDrawing == true){
-                this.x = mouseX-50;
-                this.y = mouseY-50;
-                this.affPostIT();
-            }},10);
-            });
-            document.getElementById(this.id).addEventListener('mouseup', e => {
-            this.isDrawing = false;
-            document.removeEventListener("mousedown", e)
-            clearInterval(this.monInterval);
-            });*/
             if(move == true){
                 this.x = mouseX-50;
                 this.y = mouseY-50;
                 this.affPostIT();
                 }
                 }
+    
+    /**
+     * Foncton permetant de redimensionner 1 post-it
+    */
     redimPostIT(){
-        /*document.getElementById(this.id).addEventListener('mousedown', e => {
-            this.isDrawing = true;
-            let initMouseX = mouseX;
-            let initmouseY = mouseY;
-            let initlargeur = 0;
-            let initlongeur = 0;
-            initlargeur = this.largeur;
-            initlongeur = this.longeur;
-            this.monInterval = setInterval(()=>{
-                if(this.isDrawing == true){
-                this.largeur = initlargeur + mouseX - initMouseX;
-                this.longeur = initlongeur + mouseY - initmouseY;
-                this.affPostIT();
-            }},10);
-            });
-            document.body.addEventListener('mouseup', e => {
-            this.isDrawing = false;
-            clearInterval(this.monInterval);
-            });*/
-            
             if(resize == true){
                 this.largeur = this.initlargeur + mouseX - this.initMouseX-10;
                 this.longeur = this.initlongeur + mouseY - this.initmouseY-10;
                 this.affPostIT();
             }
     }
-
-
+        /**
+     * Foncton permetant d'afficher un post-it sur le navigateur
+     * @param {string} texte texte contenu dans le post-it
+     */
 textePostIT(texte){
         this.texte = texte
-        this.affPostIT();
+        //this.affPostIT();
     }
+majPostIT(x, y, largeur, longeur, texte){
+    this.x = x;
+    this.y = y;
+    this.longeur = longeur;
+    this.largeur = largeur;
+    this.texte = texte;
+
+}
 }
--- assets/JS/index.js
+++ assets/JS/index.js
@@ -2,54 +2,116 @@ testPostIT = []
 let resize = false;
 let move = false;
 let edit = false;
-let compteur = 0;
+let id = -1;
 let mouseX = 0;
 let mouseY = 0;
 let largeur = 40;
 let longeur = 40;
+let texte = " ";
+
+let tabCookie =JSON.parse(readCookie("savePostIT"))
+
+for(let i in tabCookie){
+    console.log(tabCookie[i])
+
+    id = testPostIT.length;
+    testPostIT.push(new CPostIT (largeur,longeur,tabCookie[i].couleur,tabCookie[i].couleur2,1,1,"test2",id))
+    testPostIT[id].majPostIT(tabCookie[i].x, tabCookie[i].y, tabCookie[i].largeur, tabCookie[i].longeur, tabCookie[i].texte );
+    testPostIT[id].affPostIT();
+}
 document.body.addEventListener('click', () => {
     resize = false;
     move = false;
     edit = false;
 })
-function PosMouse(id){
+    /**
+     * Foncton permetant de savoir l'emplacement du curseur de la sourie
+    */
+function PosMouse(){
     document.body.addEventListener('mousemove', e => {
         mouseX = e.clientX;
         mouseY = e.clientY;
         if(move == true){
-            let taille = testPostIT.length;
-            for(i = 0; i < taille; i++) {
-            testPostIT[i].movePostIT();
-            }
-
+            testPostIT[id].movePostIT();
         }
         else if(resize == true){
-            let taille = testPostIT.length;
-            for(i = 0; i < taille; i++) {
-            testPostIT[i].redimPostIT();
-            }
+            testPostIT[id].redimPostIT();
         }
-        
-        
-        });
-    /*let numPostIT = -1;
-    for(i = 0; i < taille; i++) {
-        testPostIT[i].movePostIT();
-    }*/
+        else if (edit == true){
+            testPostIT[id].redimPostIT();
+        }});
 }
+    /**
+     * Foncton permetant de savoir l'emplacement du curseur de la sourie
+     * @param {string} couleur couleur primaire du post-it
+     * @param {string} couleur2 couleur secondaire du post-it
+    */
 function NewPostIT(couleur, couleur2){
-    compteur += 1;
-    testPostIT.push(new CPostIT (largeur,longeur,couleur,couleur2,1,1,"test2",compteur))
-    let taille = testPostIT.length;
-    for(i = 0; i < taille; i++) {
-        testPostIT[i].affPostIT();
-    }
+    id = testPostIT.length;
+    testPostIT.push(new CPostIT (largeur,longeur,couleur,couleur2,1,1,"test2",id))
+    testPostIT[id].affPostIT();
 }
-function recupId()
-{
-    if(this.id)
-        alert(this.id);
 
-    return false;
+document.body.addEventListener('keydown', touche)
+
+    /**
+     * Foncton permetant de savoir l'emplacement du curseur de la sourie
+     * @param {string} e évènement déclencher lors de l'appuie d'un touche
+    */
+function touche(e) {
+    if (edit == true){
+    console.log (e)
+    
+        if(e.key == " "){
+            testPostIT[id].textePostIT(testPostIT[id].texte + "&nbsp;")
+            testPostIT[id].affPostIT();
+        }
+        else if(e.key == "Shift"){
+
+        }
+        else if(e.key == "Enter"){
+            testPostIT[id].textePostIT(testPostIT[id].texte + "<br>")
+            testPostIT[id].affPostIT();
+        }
+        else if(e.key == "Backspace"){
+            testPostIT[id].textePostIT(testPostIT[id].texte.substr(0, testPostIT[id].texte.length - 1))
+            testPostIT[id].affPostIT();
+        }
+        else{
+        testPostIT[id].textePostIT(testPostIT[id].texte + e.key)
+        testPostIT[id].affPostIT();
+        }
+    }
+
+}
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/; SameSite=None; Secure";
+    
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
 }
 PosMouse();
+monTimer = setInterval(createCookie("Save",testPostIT,30),1000);
+setInterval(() => {
+    createCookie('savePostIT',JSON.stringify(testPostIT),28)
+},1000);
+
